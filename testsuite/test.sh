e174cae43146a22606ca7d51985671808ca71bba
Will Robertson <wspr81@gmail.com>
2014-05-11 17:33:04 +0930

    test one per dir

--- testsuite/test.sh
+++ testsuite/test.sh
@@ -1,24 +1,4 @@
 #!/bin/sh
 
-echo "TEST 00"
-cd ..
-pwd
-latex pstool.ins
-cp pstool.sty "testsuite/00 - plain/"
-
-cd "testsuite/00 - plain"
-git clean
-pdflatex -shell-escape "testing.tex"
-
-
-
-echo "TEST 01"
-cd ..
-pwd
-latex pstool.ins
-cp pstool.sty "testsuite/01 - beamer/"
-
-cd "testsuite/01 - beamer"
-git clean
-pdflatex -shell-escape "testing.tex"
-
+cd "00 - plain/"
+sh test.sh
