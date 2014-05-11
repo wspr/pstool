a80cba8e14646c3204f20f3322b5646fbbc1d64d
Will Robertson <wspr81@gmail.com>
2014-05-11 17:26:23 +0930

    beamer test

--- testsuite/test.sh
+++ testsuite/test.sh
@@ -7,5 +7,18 @@ latex pstool.ins
 cp pstool.sty "testsuite/00 - plain/"
 
 cd "testsuite/00 - plain"
+git clean
+pdflatex -shell-escape "testing.tex"
+
+
+
+echo "TEST 01"
+cd ..
+pwd
+latex pstool.ins
+cp pstool.sty "testsuite/01 - beamer/"
+
+cd "testsuite/01 - beamer"
+git clean
 pdflatex -shell-escape "testing.tex"
 
