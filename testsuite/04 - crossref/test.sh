a9d839ec00ab598c7c7e0ee2524e5a54f10c0e6e
Will Robertson <wspr81@gmail.com>
2014-05-11 18:35:15 +0930

    cross ref and bib test files

---
+++ testsuite/04 - crossref/test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "  "
+echo "==========="
+echo "PSTOOL TEST"
+echo "  "
+
+git clean -f
+
+x=`pwd`
+cd ../..
+pwd
+latex pstool.ins
+cp pstool.sty "$x"
+
+cd "$x"
+pwd
+pdflatex -shell-escape "testing.tex"
+pdflatex -shell-escape "testing.tex"
