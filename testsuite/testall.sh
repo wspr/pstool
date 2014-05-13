ff18bbbba37cd7ee8dbf47817c4f6ad69230652f
Will Robertson <wspr81@gmail.com>
2014-05-13 16:41:49 +0930

    improve testall to use a loop :)
    sometimes I'm lazy like that

--- testsuite/testall.sh
+++ testsuite/testall.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-(cd "00 - plain/"; sh test.sh)
+for dir in ./*/
+do
+    (cd "$dir"; sh test.sh)
+done
 
-(cd "01 - beamer/"; sh test.sh)
-
-(cd "02 - subdir/"; sh test.sh)
-
-(cd "03 - bitmaps/"; sh test.sh)
-
-(cd "04 - crossref/"; sh test.sh)
-
-(cd "05 - bib/"; sh test.sh)
