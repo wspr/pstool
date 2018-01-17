d8490ac5397d9ee34fdc6f16df3c18e89092da94
Will Robertson <wspr81@gmail.com>
2018-01-17 12:54:15 +1030

    start to implement crossref=false

---
+++ testsuite/06 - crossref disabled/test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "  "
+echo "==========="
+echo "PSTOOL TEST"
+echo "  "
+
+git clean -f
+
+x=`pwd`
+cd ../..
+pwd
+cp pstool.sty "$x"
+
+cd "$x"
+pwd
+pdflatex -shell-escape "testing.tex"
+pdflatex -shell-escape "testing.tex"
